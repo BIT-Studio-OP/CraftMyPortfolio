e187745446075061be9dc21bf7e39e5ed305137d
Durr E Muhammad Ghauri (1000103499) <GHAUD1@student.op.ac.nz>
2023-05-18 14:09:05 +1200

    fix: fixing Tailwind integration bug
    moving the project to the root of the repository

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-500 font-bold">Vite + React</h1>
+      <div className="font-bold"></div>
+    </>
+  );
+}
+
+export default App;
