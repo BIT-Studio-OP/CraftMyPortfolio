7e8df96457cd48579c0f28c81541e3da22fa87be
Durr E Muhammad Ghauri (1000103499) <GHAUD1@student.op.ac.nz>
2023-05-25 13:52:02 +1200

    feat : creating: login logout and signup page and also adding code for later for google authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { auth } from "./utils/Firestore";
+import { onAuthStateChanged } from "firebase/auth";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import SignOut from "./components/auth/SignOut";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-blue-500 font-bold">Vite + React</h1>
-      <div className="font-bold"></div>
-    </>
-  );
+  const [showSignIn, setShowSignIn] = useState(true);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const toggleForm = () => {
+    setShowSignIn(!showSignIn);
+  };
+
+    return (
+      <Router>
+        {currentUser ? <div>Welcome, {currentUser.displayName}</div> : null}
+        <div className="background">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  {currentUser ? (
+                    <>
+                    <div>Hello I am Home Page </div>
+                      <SignOut setCurrentUser={setCurrentUser} />
+                    </>
+                  ) : showSignIn ? (
+                    <SignIn toggleForm={toggleForm} />
+                  ) : (
+                    <SignUp toggleForm={toggleForm} />
+                  )}
+                </>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    );
 }
 
 export default App;
