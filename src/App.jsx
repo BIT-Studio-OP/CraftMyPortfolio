b150708a2f12299405cc41236c289c49480d4cb1
CalebStephens <calebtaylorstephens@gmail.com>
2023-06-02 20:56:06 +1200

    show and hide responsive nav menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
+    <Header />
       <h1 className="text-blue-500 font-bold">Vite + React</h1>
       <div className="font-bold"></div>
     </>
