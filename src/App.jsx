89e56cd067308e991b82a9c24da6439cdb3177bc
Durr E Muhammad Ghauri (1000103499) <GHAUD1@student.op.ac.nz>
2023-06-07 01:37:15 +1200

    feat: creating landing page and showing random images from google

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <>
-      <h1 className="text-blue-500 font-bold">Vite + React</h1>
-      <div className="font-bold"></div>
+      <Home />
     </>
   );
 }
