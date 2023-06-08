1aecbe11aa8eae1b0cfc4ac7d9dd9257e194cd16
CalebStephens <calebtaylorstephens@gmail.com>
2023-06-08 13:25:06 +1200

    commented code

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
-
+  // State variable to control the visibility of the responsive menu
   const [showMenu, setShowMenu] = useState(false);
+  // Function to handle the click event on the bars icon
   const handleIconClick = () => {
     setShowMenu(!showMenu);
   };
@@ -20,6 +21,7 @@ function Header() {
                 <li><a>Account</a></li>
                 <li className="projects-button"><a>My Projects</a></li>
             </ul>
+            {/* Responsive menu for smaller screens */}
             <div className='res-menu-parent'>
               <FontAwesomeIcon className='bars' icon={faBars} onClick={handleIconClick} />
               <ul className={ showMenu ? 'res-menu' : 'res-menu-hide'}>
