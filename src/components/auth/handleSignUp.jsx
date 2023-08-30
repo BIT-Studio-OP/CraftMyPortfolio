f05ded8f7aeb460a0de2a902b79028f99a7f1a4d
Kieren <dunnersguy@gmail.com>
2023-08-31 11:44:40 +1200

    more testing

--- src/components/auth/handleSignUp.jsx
+++ src/components/auth/handleSignUp.jsx
@@ -6,6 +6,10 @@ import {
     signInWithPopup,
     updateProfile,
   } from "firebase/auth";
+  
+import { setDoc, doc } from "firebase/firestore";
+
+import { firestore } from "../../utils/Firestore.jsx";
 
 export default handleSignUp = async (email, password, auth ) => {
     const userCredential = await createUserWithEmailAndPassword(
