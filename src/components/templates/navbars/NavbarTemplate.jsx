67d5a1dd022741bf97a6449df39ab4cbb766a66a
Durr E Muhammad Ghauri (1000103499) <GHAUD1@student.op.ac.nz>
2023-10-12 02:28:01 +1300

    chore: grid in project

--- src/components/templates/navbars/NavbarTemplate.jsx
+++ src/components/templates/navbars/NavbarTemplate.jsx
@@ -1,64 +1,7 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { createUseStyles } from "react-jss";
-
-const useStyles = createUseStyles({
-  navParent: {
-    backgroundColor: "var(--primary-colour)",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "1rem",
-  },
-  h1: {
-    fontSize: "48px !important",
-    fontWeight: 600,
-    color: "white",
-    fontFamily: "Delicious Handrawn, cursive",
-  },
-  menu: {
-    display: "flex",
-    justifyContent: "space-between",
-    gap: "1rem",
-    listStyle: "none",
-    alignItems: "center",
-    "& li": {
-      "& a": {
-        fontFamily: "Delicious Handrawn, cursive !important",
-      },
-    },
-  },
-  MenuItem: {
-    // Add your menu item styles here
-  },
-  a: {
-    textDecoration: "none",
-    color: "white",
-    fontSize: "1.2rem",
-    fontWeight: 600,
-    transition: "all 0.1s ease-in-out",
-    borderRadius: "0.5rem",
-    cursor: "pointer",
-    "&:hover": {
-      backgroundColor: "var(--white)",
-      color: "var(--accent-colour)",
-    },
-  },
-  bars: {
-    display: "none",
-  },
-  resMenuParent: {
-    display: "none",
-  },
-  resMenu: {
-    display: "none",
-  },
-  resMenuHide: {
-    display: "none",
-  },
-  // Add responsive styles here
-});
+import { useStyles } from "./NavbarTemplateStyles";
 
 function NavbarTemplate() {
   const [showMenu, setShowMenu] = useState(false);
@@ -72,18 +15,17 @@ function NavbarTemplate() {
     <>
       <nav className={classes.navParent}>
         <h1 className={classes.h1}>
-          <span className={classes.craft}>Craft</span>My
-          <span className={classes.portfolio}>Portfolio</span>
+          <span className={classes.craft}>YourTitle</span>
         </h1>
         <ul className={classes.menu}>
           {/* Editable menu items */}
           <li className={classes.MenuItem}>
-            <a href="/" className={classes.a}>
+            <a href="" className={classes.a}>
               Home
             </a>
           </li>
           <li className={classes.MenuItem}>
-            <a href="/templates" className={classes.a}>
+            <a href="" className={classes.a}>
               Templates
             </a>
           </li>
@@ -91,12 +33,12 @@ function NavbarTemplate() {
             <a className={classes.a}>Account</a>
           </li>
           <li className={classes.MenuItem}>
-            <a href="/details" className={classes.a}>
+            <a href="" className={classes.a}>
               Details
             </a>
           </li>
           <li className={`${classes.MenuItem} projects-button`}>
-            <a href="/projects" className={classes.a}>
+            <a href="" className={classes.a}>
               My Projects
             </a>
           </li>
@@ -114,17 +56,17 @@ function NavbarTemplate() {
           <ul className={showMenu ? classes.resMenu : classes.resMenuHide}>
             {/* Editable responsive menu items */}
             <li className={classes.MenuItem}>
-              <a href="/" className={classes.a}>
+              <a href="" className={classes.a}>
                 Home
               </a>
             </li>
             <li className={classes.MenuItem}>
-              <a href="/templates" className={classes.a}>
+              <a href="" className={classes.a}>
                 Templates
               </a>
             </li>
             <li className={classes.MenuItem}>
-              <a href="/details" className={classes.a}>
+              <a href="" className={classes.a}>
                 Details
               </a>
             </li>
@@ -132,7 +74,7 @@ function NavbarTemplate() {
               <a className={classes.a}>Account</a>
             </li>
             <li className={`${classes.MenuItem} projects-button`}>
-              <a href="/projects" className={classes.a}>
+              <a href="" className={classes.a}>
                 My Projects
               </a>
             </li>
--- src/pages/ProjectEdit.jsx
+++ src/pages/ProjectEdit.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { createUseStyles } from "react-jss";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import TemplateFooterOne from "../components/templates/footers/templateFooterOne";
 import NavbarTemplate from "../components/templates/navbars/NavbarTemplate";
+import { useStyles as useNavbarStyles } from "../components/templates/navbars/NavbarTemplateStyles";
+import ReactDOMServer from "react-dom/server";
 
 const useStyles = createUseStyles({
   container: {
@@ -11,7 +13,7 @@ const useStyles = createUseStyles({
     height: "100%",
     minHeight: "100vh",
     display: "flex",
-    flexDirection: "column", // Make it a column layout
+    flexDirection: "column",
     color: "black",
   },
   rightSidebar: {
@@ -22,10 +24,10 @@ const useStyles = createUseStyles({
     flexDirection: "column",
   },
   leftEditor: {
-    flex: 2, // Make the left side larger
+    flex: 2,
     padding: "20px",
     display: "flex",
-    flexDirection: "column", // Make it a column layout
+    flexDirection: "column",
   },
   template: {
     marginBottom: "20px",
@@ -36,7 +38,7 @@ const useStyles = createUseStyles({
   },
   placeholder: {
     height: "300px",
-    backgroundColor: "red",
+    backgroundColor: "gray",
     border: "1px dashed #ccc",
     display: "flex",
     alignItems: "center",
@@ -49,21 +51,122 @@ const useStyles = createUseStyles({
     minHeight: "300px",
     margin: "10px",
     padding: "10px",
-    flex: 1, // Allow the footer to take up the remaining space
+    flex: 1,
     display: "flex",
-    flexDirection: "column", // Make it a column layout
+    flexDirection: "column",
   },
   footer: {
-    flex: 1, // Allow the footer to take up the remaining space
+    flex: 1,
+  },
+  bodyContent: {
+    flex: 1,
+    backgroundColor: "white",
+    border: "1px solid #ccc",
+    padding: "20px",
+  },
+  bodyOption: {
+    marginBottom: "10px",
+  },
+  gridContainer: {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(650px, 1fr))",
+    gap: "20px",
+  },
+  gridItem: {
+    backgroundColor: "lightgray",
+    padding: "10px",
+    border: "1px solid #ccc",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+
+  gridImage: {
+    width: "100%",
+    height: "100px", // Set the desired image height
+    objectFit: "cover",
+    marginBottom: "10px",
+  },
+  gridDescription: {
+    textAlign: "center",
+    padding: "5px",
+    border: "1px dashed #ccc",
+  },
+  select: {
+    display: "flex",
+    width: "100%",
+    padding: "10px",
+    marginBottom: "10px",
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+    fontSize: "16px",
+    color: "#444",
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    marginBottom: "10px",
+  },
+  droppableArea: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+  },
+  descriptionModal: {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    background: "rgba(0, 0, 0, 0.5)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 100,
+  },
+  modalContent: {
+    background: "white",
+    padding: "20px",
+    borderRadius: "5px",
+  },
+  button: {
+    width: "100%",
+    padding: "0.5rem",
+    fontSize: "1rem",
+    fontWeight: "bold",
+    color: "#fff",
+    background: "#3b82f6",
+    borderRadius: "0.25rem",
+    boxShadow: "0 0 0.25rem rgba(0, 0, 0, 0.5)",
+    transition: "background 0.2s ease-in-out",
+    "&:hover": {
+      background: "#2563eb",
+    },
+    "&:focus": {
+      outline: "none",
+      boxShadow: "0 0 0.5rem rgba(0, 0, 0, 0.5)",
+    },
+  },
+  header: {
+    backgroundColor: "lightblue",
+    padding: "10px",
+    borderBottom: "1px solid #ccc",
   },
 });
 
 const ProjectEdit = () => {
   const classes = useStyles();
   const [selectedHeaderTemplates, setSelectedHeaderTemplates] = useState([]);
-  const [selectedBodyTemplates, setSelectedBodyTemplates] = useState([]);
   const [selectedFooterTemplates, setSelectedFooterTemplates] = useState([]);
-
+  const [selectedBodyOption, setSelectedBodyOption] = useState("blank");
+  const [gridRows, setGridRows] = useState(3);
+  const [gridCols, setGridCols] = useState(1);
+  const [gridDescriptions, setGridDescriptions] = useState(
+    Array.from({ length: gridRows * gridCols }, () => "")
+  );
   const handleHeaderTemplateSelect = (e) => {
     const templateName = e.target.value;
     if (!selectedHeaderTemplates.includes(templateName)) {
@@ -71,29 +174,20 @@ const ProjectEdit = () => {
     }
   };
 
-  const handleBodyTemplateSelect = (e) => {
-    const templateName = e.target.value;
-    if (!selectedBodyTemplates.includes(templateName)) {
-      setSelectedBodyTemplates([...selectedBodyTemplates, templateName]);
-    }
-  };
-
-  const handleFooterTempalteSelect = (e) => {
+  const handleFooterTemplateSelect = (e) => {
     const templateName = e.target.value;
     if (!selectedFooterTemplates.includes(templateName)) {
       setSelectedFooterTemplates([...selectedFooterTemplates, templateName]);
     }
   };
 
+  const handleBodyOptionSelect = (e) => {
+    setSelectedBodyOption(e.target.value);
+  };
+
   const removeHeaderTemplate = (templateName) => {
     setSelectedHeaderTemplates(
       selectedHeaderTemplates.filter((template) => template !== templateName)
-    );
-  };
-
-  const removeBodyTemplate = (templateName) => {
-    setSelectedBodyTemplates(
-      selectedBodyTemplates.filter((template) => template !== templateName)
     );
   };
 
@@ -103,35 +197,166 @@ const ProjectEdit = () => {
     );
   };
 
+  const handleGridRowsChange = (e) => {
+    const newGridRows = parseInt(e.target.value, 10);
+    setGridRows(newGridRows);
+
+    // If the number of rows is increased, add descriptions for new grid items
+    if (newGridRows > gridRows) {
+      setGridDescriptions((descriptions) => [
+        ...descriptions,
+        ...Array.from(
+          { length: gridCols * (newGridRows - gridRows) },
+          () => ""
+        ),
+      ]);
+    }
+  };
+
+  const handleGridColsChange = (e) => {
+    setGridCols(parseInt(e.target.value, 10));
+  };
+
+  const generateHTML = () => {
+    // Render the selected header templates
+    const headerContent = selectedHeaderTemplates
+      .map((templateName) => {
+        switch (templateName) {
+          case "HeaderTemplateOne":
+            return ReactDOMServer.renderToString(<NavbarTemplate />);
+          // Add more cases as needed
+          default:
+            return "";
+        }
+      })
+      .join("");
+
+    // Render the selected footer templates
+    const footerContent = selectedFooterTemplates
+      .map((templateName) => {
+        switch (templateName) {
+          case "FooterTemplateOne":
+            return ReactDOMServer.renderToString(<TemplateFooterOne />);
+          // Add more cases as needed
+          default:
+            return "";
+        }
+      })
+      .join("");
+
+    // Define your HTML content
+    const htmlContent = `
+      <html>
+      <head>
+        <style>
+          /* Add your CSS styles here */
+          .header { background-color: lightblue; padding: 10px; border-bottom: 1px solid #ccc; }
+          .footer { background-color: lightgray; padding: 10px; border-top: 1px solid #ccc; }
+          /* ...other styles... */
+          </style>
+          </head>
+          <body>
+            <div class="header">
+              ${headerContent}
+            </div>
+            <div class="body">
+              ${
+                selectedBodyOption === "blank"
+                  ? `<div class="placeholder">Blank White Body (default)</div>`
+                  : selectedBodyOption === "grid"
+                  ? `<div class="gridContainer">
+                  ${Array.from(
+                    { length: gridRows * gridCols },
+                    (_, index) => `
+                    <div class="gridItem">
+                      <img src="" alt="Grid Item Image" class="gridImage" />
+                      <p class="gridDescription">${gridDescriptions[index]}</p>
+                    </div>
+                  `
+                  ).join("")}
+                </div>`
+                  : selectedBodyOption === "carousel"
+                  ? `<div class="bodyOption">Carousel content</div>`
+                  : ""
+              }
+            </div>
+            <div class="footer">
+              ${footerContent}
+            </div>
+          </body>
+          </html>
+        `;
+
+    // Create a Blob with the HTML content
+    const blob = new Blob([htmlContent], { type: "text/html" });
+
+    // Create a URL for the Blob
+    const url = URL.createObjectURL(blob);
+
+    // Create a link for downloading the HTML file
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "generated-page.html";
+    a.click();
+  };
+
   return (
     <>
       <Header />
       <div className={classes.container}>
         <div className={classes.rightSidebar}>
           <h3>Select Header Templates</h3>
-          <select onChange={handleHeaderTemplateSelect}>
+          <select
+            onChange={handleHeaderTemplateSelect}
+            className={classes.select}
+          >
             <option value="none">Select a header template...</option>
             <option value="HeaderTemplate">Header Template</option>
             {/* Add more header templates as needed */}
           </select>
-          <h3>Select Body Templates</h3>
-          <select onChange={handleBodyTemplateSelect}>
-            <option value="none">Select a body template...</option>
-            <option value="BodyTemplate">Body Template</option>
-            {/* Add more body templates as needed */}
-          </select>
-          <h3>Select footer Templates</h3>
-          <select onChange={handleFooterTempalteSelect}>
+          <h3>Select Footer Templates</h3>
+          <select
+            onChange={handleFooterTemplateSelect}
+            className={classes.select}
+          >
             <option value="none">Select a footer template...</option>
             <option value="FooterTemplate">Footer Template</option>
           </select>
+          <h3>Select Body Options</h3>
+          <select onChange={handleBodyOptionSelect} className={classes.select}>
+            <option value="blank">Blank Body</option>
+            <option value="grid">Grid</option>
+            <option value="carousel">Carousel</option>
+            {/* Add more body options as needed */}
+          </select>
+          {selectedBodyOption === "grid" && (
+            <>
+              <label>Number of Rows:</label>
+              <input
+                type="number"
+                min={3}
+                value={gridRows}
+                onChange={handleGridRowsChange}
+                className={classes.select}
+              />
+              <label>Number of Columns:</label>
+              <input
+                type="number"
+                min={1}
+                value={gridCols}
+                onChange={handleGridColsChange}
+                className={classes.select}
+              />
+            </>
+          )}
         </div>
         <div className={classes.leftEditor}>
           <div className={classes.footer}>
             <div className={classes.text}>Edit Project Screen</div>
             <div className={classes.droppableArea}>
               <div className={classes.text}>Header Templates Goes Here</div>
-              <div className={classes.dropArea}>
+              <div className={`${classes.dropArea} ${classes.navParent}`}>
+                {" "}
                 {selectedHeaderTemplates.map((templateName) => (
                   <NavbarTemplate
                     key={templateName}
@@ -140,15 +365,35 @@ const ProjectEdit = () => {
                   />
                 ))}
               </div>
-              <div className={classes.text}>Body Templates Goes Here</div>
-              <div className={classes.dropArea}>
-                {selectedBodyTemplates.map((templateName) => (
-                  <TemplateFooterOne
-                    key={templateName}
-                    templateName={templateName}
-                    removeTemplate={removeBodyTemplate}
-                  />
-                ))}
+              <div className={classes.text}>Body Content Goes Here</div>
+              <div className={classes.bodyContent}>
+                {selectedBodyOption === "blank" && (
+                  <div className={classes.placeholder}>
+                    Blank White Body (default)
+                  </div>
+                )}
+                {selectedBodyOption === "grid" && (
+                  <div className={classes.gridContainer}>
+                    {Array.from({ length: gridRows * gridCols }, (_, index) => (
+                      <div key={index} className={classes.gridItem}>
+                        <img
+                          src=""
+                          alt="Grid Item Image"
+                          className={classes.gridImage}
+                        />
+                        <p className={classes.gridDescription}>
+                          {gridDescriptions[index]}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                )}
+
+                {selectedBodyOption === "carousel" && (
+                  <div className={classes.bodyOption}>
+                    {/* Render your carousel content here */}
+                  </div>
+                )}
               </div>
               <div className={classes.text}>Footer Templates Goes Here</div>
               <div className={classes.dropArea}>
@@ -163,6 +408,9 @@ const ProjectEdit = () => {
             </div>
           </div>
         </div>
+        <button className={classes.button} onClick={generateHTML}>
+          Generate HTML
+        </button>
       </div>
       <Footer />
     </>
